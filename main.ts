66d91d5d114bc1d42ee4948bcbc4772bc405802a
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-31 16:29:28 +0530

    tried yt plan

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
+
 require("./index");
 const { app, BrowserWindow, ipcMain, nativeTheme, webContents} = require('electron')
 const path = require('path')
 const Spotify = require("erela.js-spotify");
 const Deezer = require("erela.js-deezer");
 const FaceBook = require("erela.js-facebook");
+const yas = require('youtube-audio-server')
 class trip{
 constructor(){
   this.manage = null;
 }
 }
+
 const axios = require("axios")
 const { Manager } = require("erela.js");
+async function useAPI() {
+  const youtube = new Youtube("AIzaSyAoKdUjrLI5Hsmsxp1IR3WA7N_yzub0B78")
+  const key = "AIzaSyAoKdUjrLI5Hsmsxp1IR3WA7N_yzub0B78"
+  const cookie = "your long browser cookie"
+  await youtube.util.downloadMP3("https://www.youtube.com/watch?v=mLJQ0HO5Alc", "./videos/mp3")
+}
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
@@ -60,10 +69,21 @@ trip.manage=new Manager({
   })
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(async () => {
   createWindow()
- 
-    
+  const id = 'HQmmM_qwG4k' // "Whole Lotta Love" by Led Zeppelin.
+  const file = 'whole-lotta-love.mp3'
+  console.log(`Downloading ${id} into ${file}...`)
+  yas.downloader
+    .setFolder('some/folder') // Optionally set a folder for downloaded content.
+    .onSuccess(({id, file}) => {
+      console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`)
+    })
+    .onError(({ id, file, error }) => {
+      console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
+    })
+    .download({ id, file})
+
   const contents = webContents.getAllWebContents()[0]
 
   // The WebContents class has dozens of methods and
@@ -80,6 +100,7 @@ app.whenReady().then(() => {
 })
 
 app.on('window-all-closed', () => {
+  
   if (process.platform !== 'darwin') {
     app.quit()
   }
