4a155cc065f00145203e30e08f26f6a2c3024d3e
PRIYANSHU <tpriyanshu775@gmail.com>
2022-01-31 15:07:09 +0530

    js -> tsx

---
+++ renderer.tsx
@@ -0,0 +1,19 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+  })
+  const webview = document.querySelector('webview')
+webview.addEventListener('dom-ready', () => {
+  webview.openDevTools()
+})
+
+  document.getElementById('reset-to-system').addEventListener('click', async () => {
+    await window.darkMode.system()
+    document.getElementById('theme-source').innerHTML = 'System'
+  })
