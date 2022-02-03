cfe21e88ec8058731985f34d6057c01e2abb2a2a
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-03 21:42:53 +0530

    discord-presence added

--- discord_presence.js
+++ discord_presence.js
@@ -1,6 +1,41 @@
-const Cordy = require("cordy")
-new Cordy().starter("ODczNTY0NjgzMTA0MzEzMzY0.YQ6Qgg.Fh2iL-9Dym772Rar-5cdtQq0hBQ",{activity:[{
-    status:"Combined with electron Js",
-    statusType:"PLAYING",
-    statusVisibility:"DND"}
-],timer:5000});
+const clientId = '938822371207438476';
+const DiscordRPC = require('discord-rpc');
+
+// Only needed if you want to use spectate, join, or ask to join
+DiscordRPC.register(clientId);
+const path = require('path');
+const url = require('url');
+
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+ 
+
+  const boops = 2;
+
+  // You'll need to have snek_large and snek_small assets uploaded to
+  // https://discord.com/developers/applications/<application_id>/rich-presence/assets
+  rpc.setActivity({
+    details: `Stage ${boops}`,
+    state: 'in Development',
+    startTimestamp,
+    largeImageKey: 'snek_large',
+    largeImageText: 'Beta Mode',
+    smallImageKey: 'snek_small',
+    smallImageText: 'Electron',
+    instance: false,
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  console.log("ready");
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
