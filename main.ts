38421719ed055eb67a462937521c3ffb4a0a9e13
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-03 23:48:23 +0530

    updated logo

--- main.ts
+++ main.ts
@@ -12,6 +12,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, './images/snek_large.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
