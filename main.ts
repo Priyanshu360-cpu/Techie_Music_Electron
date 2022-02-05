7faeaebc35a2da035ca8c4d0cc77ca2354cc9124
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-05 13:33:21 +0530

    yt method applied

--- main.ts
+++ main.ts
@@ -35,27 +35,33 @@ function createWindow () {
     nativeTheme.themeSource = 'system'
   })
 }
-ipcMain.handle('download', (event,x) => {
-  const id = x
-  const file = 'whole-lotta-love.mp3'
-  console.log(`Downloading ${id} into ${file}...`)
-  yas.downloader
-    .setFolder('some/folder') 
-    .onSuccess(({id, file}) => {
-      sound.play(path.join(__dirname, './some/folder/youtube-audio.mp3'));
-      console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`)
-    })
-    .onError(({ id, file, error }) => {
-      console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
-    })
-    .download({ id, file})
-    event.returnValue = 'Main said I received your Sync message';
-})
-ipcMain.on('asyncChannelToMain', (event, arg) => {
-  console.log(arg + ' from renderer')
-  if (arg === 'hello') {
-    event.sender.send('asyncChannelToRenderer', 'world')
-  }
+ipcMain.handle('download', async (event,x) => {
+ 
+  let y = x.replace(' ','+');
+  const url = "https://www.googleapis.com/youtube/v3/search/?key=AIzaSyCSxMvPgYvu45ORWdHkoTdgFqE3Vvn0Mik&part=snippet&q="+y;
+  console.log(url);
+            let response, data;
+            try {
+                response = await axios.get(url);
+                data = response.data;
+            } catch (e) {
+                return console.log(`An error occured!`)
+            }
+            const id = data.items[0].id.videoId;
+            const file = 'whole-lotta-love.mp3'
+            console.log(`Downloading ${id} into ${file}...`)
+            yas.downloader
+              .setFolder('some/folder') 
+              .onSuccess(({id, file}) => {
+                sound.play(path.join(__dirname, './some/folder/youtube-audio.mp3'));
+                console.log(`Yay! Audio (${id}) downloaded successfully into "${file}"!`)
+              })
+              .onError(({ id, file, error }) => {
+                console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
+              })
+              .download({ id, file})
+              event.returnValue = 'Main said I received your Sync message';
+  
 })
 
 app.whenReady().then(async () => {
