3d214e51e3cf8e47fd8adde64bc57eb8d3421927
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-05 14:11:52 +0530

    imported discord client

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 const sound = require("sound-play");
-const a = require("./discord_presence");
+const Discord = require("./discord_presence");
 const { app, BrowserWindow, ipcMain, nativeTheme, webContents} = require('electron')
 const path = require('path')
 const yas = require('youtube-audio-server')
@@ -35,8 +35,22 @@ function createWindow () {
     nativeTheme.themeSource = 'system'
   })
 }
+
+async function setActivity(largeimage,largetext,detail) {
+  const startTimestamp = new Date();
+  Discord.rpc.setActivity({
+    details: largetext,
+    state: detail,
+    startTimestamp,
+    largeImageKey: largeimage,
+    largeImageText: "Techie Electron",
+    smallImageKey: 'snek_large',
+    smallImageText: 'Beta Mode',
+    instance: false,
+  });
+}
 ipcMain.handle('download', async (event,x) => {
- 
+  
   let y = x.replace(' ','+');
   const url = "https://www.googleapis.com/youtube/v3/search/?key=AIzaSyCSxMvPgYvu45ORWdHkoTdgFqE3Vvn0Mik&part=snippet&q="+y;
   console.log(url);
@@ -47,6 +61,7 @@ ipcMain.handle('download', async (event,x) => {
             } catch (e) {
                 return console.log(`An error occured!`)
             }
+            setActivity(data.items[0].snippet.thumbnails.high.url,data.items[0].snippet.title,data.items[0].snippet.channelTitle);
             const id = data.items[0].id.videoId;
             const file = 'whole-lotta-love.mp3'
             console.log(`Downloading ${id} into ${file}...`)
