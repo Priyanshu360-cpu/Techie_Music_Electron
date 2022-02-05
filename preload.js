97a182dd1d9a5c19fe6504d89ae64677ab367722
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-05 14:12:02 +0530

    removed cache

--- preload.js
+++ preload.js
@@ -1,5 +1,4 @@
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
+
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('darkMode', {
