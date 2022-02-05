91ab08f1dd17f1fe9b18a4ec6f9770aa5860c418
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-05 16:34:23 +0530

    linkedlist implemented for stroing played songs

---
+++ ai_recommender.js
@@ -0,0 +1,12 @@
+class Head{
+    constructor(){
+        this.head=null;
+    }
+}
+class List{
+    constructor(x){
+        this.value=x;
+        this.next=null;
+    }
+}
+const a = new Head();
