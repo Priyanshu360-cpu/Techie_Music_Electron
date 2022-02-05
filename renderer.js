4a804eb2f2d93ff1571b636cf4560d7ad2462de6
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-05 14:12:06 +0530

    removed cache

--- renderer.js
+++ renderer.js
@@ -1,9 +1,4 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+
 
 document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
     const isDarkMode = await window.darkMode.toggle()
