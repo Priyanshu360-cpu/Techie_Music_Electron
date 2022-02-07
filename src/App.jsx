9ca9d1238aa1383356f403e342011dda5032c174
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-08 03:17:41 +0530

    added a method to play music

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import blank from './images/nomusic.jpg'
+import techie from './some/folder/techiehi.mp3'
+import React, { useState, useEffect, Component }  from "react";
 
-function App() {
+class App extends Component{
+  state = {
+    audio: new Audio(techie),
+    isPlaying: false,
+  };
+  playPause = () => {
+
+    // Get state of song
+    let isPlaying = this.state.isPlaying;
+
+    if (isPlaying) {
+      this.state.audio.pause();
+    } else {
+      this.state.audio.play();
+      console.log("played")
+    }
+    this.setState({ isPlaying: !isPlaying });
+  };
+  render(){
   return (
     <body>
-   
     <h1>Main Window</h1>
    <themes>Test</themes>
-    <image src="./images/nomusic.jpg" id="picture"></image>
     <div id="title">Title</div>
+    <img src={blank}></img>
     <div id="album">Album</div>
     <p>Current theme source: <strong id="theme-source">System</strong></p>
     
 <textarea id = "test"></textarea>
-<button id="subo">submit</button>
+<button onClick={this.playPause}>
+         Play
+        </button>
+
     <button id="toggle-dark-mode">Toggle Dark Mode</button>
     <button id="reset-to-system">Reset to System Theme</button>
     <webview src="https://www.github.com/" plugins></webview>
     <script src="renderer.js"></script>
   </body>
   );
+  }
 }
 
 export default App;
