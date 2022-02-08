3e3388d8fef199796fad116f927bd1107620bd9e
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-09 01:04:56 +0530

    express api mounted with yt

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+const path = require('path')
+const yas = require('youtube-audio-server')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.originalUrl.replace("/",""));
+  let x = req.originalUrl.replace("/","");
   res.render('index', { title: req.originalUrl.replace("/","")});
+  let y = x.replace(' ','+');
+  const url = "https://www.googleapis.com/youtube/v3/search/?key=AIzaSyCSxMvPgYvu45ORWdHkoTdgFqE3Vvn0Mik&part=snippet&q="+y;
+  console.log(url);
+            
+            const id = x;
+            const file = 'whole-lotta-love.mp3'
+            console.log(`Downloading ${id} into ${file}...`)
+            yas.downloader
+              .setFolder('C:\\Users\\KIIT\\Desktop\\techie_music_electron\\src\\some\\folder') 
+              .onSuccess(({id, file}) => {
+                
+                console.log(`downloade file`)
+              
+              })
+              .onError(({ id, file, error }) => {
+                console.error(`Sorry, an error ocurred when trying to download ${id}`, error)
+              })
+              .download({ id, file})
 });
 
 module.exports = router;
