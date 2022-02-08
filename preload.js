78a8e0fb6e0949fad024571bc5d1532e0aadad97
PRIYANSHU <tpriyanshu775@gmail.com>
2022-02-08 22:35:04 +0530

    electron default settings done

---
+++ preload.js
@@ -0,0 +1,8 @@
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system'),
+  download: (x) => ipcRenderer.invoke('download',x)
+})
---
+++ renderer.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
+    const isDarkMode = await window.darkMode.toggle()
+    document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
+  })
+  const webview = document.querySelector('webview')
+webview.addEventListener('dom-ready', () => {
+  webview.openDevTools()
+})
+
+  document.getElementById('reset-to-system').addEventListener('click', async () => {
+    await window.darkMode.system()
+    document.getElementById('theme-source').innerHTML = 'System'
+  })
+  document.getElementById('subo').addEventListener('click', async () => {
+   
+    let d = document.getElementById('test').value;
+    
+    let e = await window.darkMode.download(d) 
+  console.log(e)})
+    
